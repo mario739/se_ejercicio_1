cbd4a535c71c81ededa0bbbcf48f7675c73e933e
Mario Aguilar <fernandoaguilar731010@gmail.com>
2021-07-06 15:19:13 -0700

    Implementar funcion de secuencia de leds

--- src/Ejercicio_1.c
+++ src/Ejercicio_1.c
@@ -7,6 +7,8 @@
 /*=====[Inclusions of function dependencies]=================================*/
 
 #include "Ejercicio_1.h"
+#include<stdio.h>
+#include<stdlib.h>
 //#include "sapi.h"
 
 /*=====[Definition macros of private constants]==============================*/
@@ -18,7 +20,11 @@
 /*=====[Definitions of private global variables]=============================*/
 
 /*=====[Main function, program entry point after power on or reset]==========*/
-
+gpioMap_t secuencia[]={LEDB,LED1,LED2,LED3};
+gpioMap_t secuencia1[]={LED3,LED2,LED1,LEDB};;
+gpioMap_t secuencia2[]={LED1,LED3,LEDB,LED2};
+gpioMap_t * psecuencia = secuencia1;
+uint8_t i = 0;
 int main( void )
 {
    // ----- Setup -----------------------------------
@@ -31,15 +37,20 @@ int main( void )
 
 	   //encenderLed(LED1);
 	  // printf("led prendido");
-	   if(!leerTecla(TEC1))
+	 /*  if(!leerTecla(TEC1))
 	   {
 		   apagarLeds();
-		   delayWrite( &delay, 150 );
+		  //delayWrite( &delay, 150 );
 	   }
 	   else
 	   {
 		   encenderLed(LED3);
 	   }
+
+*/
+	   //delayWrite( &delay, 150 );
+	  // activarSecuencia(secuencia2);
+	   activarSecuencia(psecuencia);
 
    }
 
@@ -52,6 +63,13 @@ int main( void )
 bool_t encenderLed(gpioMap_t led)
 {
 	gpioWrite(led, ON );
+	/* if(delayRead(&delay))
+	 {
+		 delayWrite( &delay, 150 );
+		 return 1;
+	 }*/
+	//return 1;
+	delay(100);
 	return 1;
 }
 bool_t apagarLeds()
@@ -68,11 +86,50 @@ bool_t leerTecla(gpioMap_t tecla)
 	return gpioRead(tecla);
 }
 
-void activarSecuencia(gpioMap_t *activarsecuencia)
+void activarSecuencia(gpioMap_t activarsecuencia[])
+{
+	for(i=0;i<=3;i++)
+	{
+		//encenderLed(activarsecuencia[i]);
+		if(i==0)
+		{
+			gpioWrite(activarsecuencia[0], ON );
+			gpioWrite(activarsecuencia[1], OFF );
+			gpioWrite(activarsecuencia[2], OFF );
+			gpioWrite(activarsecuencia[3], OFF );
+		}
+		if(i==1)
+		{
+			gpioWrite(activarsecuencia[0], OFF );
+		    gpioWrite(activarsecuencia[1], ON );
+			gpioWrite(activarsecuencia[2], OFF );
+			gpioWrite(activarsecuencia[3], OFF );
+	    }
+		if(i==2)
+	    {
+			gpioWrite(activarsecuencia[0], OFF );
+			gpioWrite(activarsecuencia[1], OFF );
+			gpioWrite(activarsecuencia[2], ON );
+			gpioWrite(activarsecuencia[3], OFF );
+	    }
+		if(i==3)
+		{
+			gpioWrite(activarsecuencia[0], OFF);
+			gpioWrite(activarsecuencia[1], OFF );
+			gpioWrite(activarsecuencia[2], OFF );
+			gpioWrite(activarsecuencia[3], ON );
+		}
+		delay(400);
+		//activarsecuencia++;
+	}
+	i=0;
+	return;
+}
+
+void prender_secuencia()
 {
 
 }
-
 
 
 
